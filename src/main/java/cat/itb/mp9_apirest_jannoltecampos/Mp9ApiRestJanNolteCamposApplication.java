6122d3addf5ee00d7494ea06f1016a3b5038558f
JanNolte <jan.nolte.7e4@itb.cat>
2022-03-28 17:21:20 +0200

    Starting Commit

---
+++ src/main/java/cat/itb/mp9_apirest_jannoltecampos/Mp9ApiRestJanNolteCamposApplication.java
@@ -0,0 +1,13 @@
+package cat.itb.mp9_apirest_jannoltecampos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp9ApiRestJanNolteCamposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mp9ApiRestJanNolteCamposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/itb/mp9_apirest_jannoltecampos/Mp9ApiRestJanNolteCamposApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itb.mp9_apirest_jannoltecampos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp9ApiRestJanNolteCamposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
